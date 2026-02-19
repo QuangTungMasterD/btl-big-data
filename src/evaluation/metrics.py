3451018b89ad637c30ed5078c3d99a6cfecb64a6
tranq <tranquangtung26062005@gmail.com>
2026-02-19 18:52:10 +0700

    add file metrics

---
+++ src/evaluation/metrics.py
@@ -0,0 +1,18 @@
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+    average_precision_score,
+)
+
+def classification_metrics(y_true, y_pred, y_prob):
+    return {
+        "Accuracy": accuracy_score(y_true, y_pred),
+        "Precision": precision_score(y_true, y_pred, zero_division=0),
+        "Recall": recall_score(y_true, y_pred, zero_division=0),
+        "F1": f1_score(y_true, y_pred, zero_division=0),
+        "ROC_AUC": roc_auc_score(y_true, y_prob),
+        "PR_AUC": average_precision_score(y_true, y_prob),
+    }
